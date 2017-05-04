af86f60cb5498640dba5dba4428b24ffdeebdb33
Jess Neary <Caneary@hotmail.com>
2017-05-05 11:28:43 +1200

    Added react-bootstrap and style.css file

--- src/components/SupplierDetails.tsx
+++ src/components/SupplierDetails.tsx
@@ -5,6 +5,7 @@ import Description from './Description'
 import ContactInfo from './ContactInfo'
 import Images from './Images'
 import ServicesList from './ServicesList'
+import { Grid, Row, Col } from 'react-bootstrap'
 
 import { Supplier } from '../interfaces'
 
@@ -12,15 +13,17 @@ export default class SupplierDetails extends React.Component<{supplier: Supplier
   render() {
     const supplier = this.props.supplier
     return (
-      <div className='SupplierDetails'>
+      <Grid className='SupplierDetails'>
         <h1 className='name'>
           <EditableDiv text={supplier.name} />
         </h1>
-        <Description text={supplier.description} />
-        <ContactInfo supplier={supplier} />
+        <Row>
+          <Description text={supplier.description} />
+          <ContactInfo supplier={supplier} />
+        </Row>
         <Images images={supplier.images} />
         <ServicesList services={supplier.services} />
-      </div>
+      </Grid>
     )
   }
 }
